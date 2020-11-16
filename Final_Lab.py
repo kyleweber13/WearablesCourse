930146ecb41983282b6f725b65c6de18e3a21c64
Kyle Weber <kyle.weber@uwaterloo.ca>
2020-11-16 09:53:52 -0500

    Added intensity comparison and daily/hourly activity graphs

--- Final_Lab.py
+++ Final_Lab.py
@@ -2,7 +2,7 @@ from matplotlib import pyplot as plt
 import numpy as np
 import pandas as pd
 import statistics
-import scipy.stats as stats
+import scipy.stats
 from datetime import datetime
 import matplotlib.dates as mdates
 import pyedflib
@@ -43,14 +43,19 @@ class Subject:
 
     def import_processed_data(self):
 
+        print("\nImporting data from processed...")
+
         self.df_epoch = pd.read_csv(self.filename)
-        self.df_epoch["Timestamp"] = pd.to_datetime(self.df_epoch["Timestamp"])
+        self.df_epoch["Timestamp"] = [pd.to_datetime(self.df_epoch["Timestamp"].iloc[0]) + timedelta(seconds=15*i) for
+                                      i in range(0, self.df_epoch.shape[0])]
 
         if self.event_filename is not None:
             self.df_event = pd.read_csv(self.event_filename)
 
             self.df_event["Start"] = pd.to_datetime(self.df_event["Start"])
             self.df_event["Stop"] = pd.to_datetime(self.df_event["Stop"])
+
+        print("Complete.")
 
     def flag_nonwear(self):
 
@@ -177,16 +182,188 @@ class Subject:
             if row.Event == "Sleep" and (start <= row.Start <= stop or start <= row.Stop <= stop):
                 ax1.fill_betweenx(x1=row.Start, x2=row.Stop, y=[0, max(self.df_epoch["LWrist"])],
                                   color='navy', alpha=.5)
-                ax2.fill_betweenx(x1=row.Start, x2=row.Stop, y=[0, max(self.df_epoch["LWrist"])],
+                ax2.fill_betweenx(x1=row.Start, x2=row.Stop, y=[40, 200],
                                   color='navy', alpha=.5)
             if row.Event == "Nonwear" and (start <= row.Start <= stop or start <= row.Stop <= stop):
                 ax1.fill_betweenx(x1=row.Start, x2=row.Stop, y=[0, max(self.df_epoch["LWrist"])],
                                   color='grey', alpha=.5)
-                ax2.fill_betweenx(x1=row.Start, x2=row.Stop, y=[0, max(self.df_epoch["LWrist"])],
+                ax2.fill_betweenx(x1=row.Start, x2=row.Stop, y=[40, 200],
                                   color='grey', alpha=.5)
 
         window_len = (stop - start).total_seconds()
         ax2.set_xlim(start + timedelta(seconds=-window_len/20), stop + timedelta(seconds=window_len/20))
+
+    def compare_wrist_hr_intensity(self, start=None, stop=None, remove_invalid_hr=False):
+
+        if start is not None and stop is not None:
+            df = self.df_epoch.loc[(self.df_epoch["Timestamp"] >= start) & (self.df_epoch["Timestamp"] <= stop)].copy()
+            start = pd.to_datetime(start)
+            stop = pd.to_datetime(stop)
+
+        if start is None and stop is None:
+            start = self.df_epoch["Timestamp"].iloc[0]
+            stop = self.df_epoch["Timestamp"].iloc[-1]
+            df = self.df_epoch.copy()
+
+        if remove_invalid_hr:
+            edited_wrist = []
+            for row in df.itertuples():
+                if row.ECG_Validity == "Valid":
+                    edited_wrist.append(row.Wrist_Intensity)
+                if row.ECG_Validity == "Invalid":
+                    edited_wrist.append(None)
+            df["Wrist_Intensity"] = edited_wrist
+
+        fig, (ax1, ax2) = plt.subplots(2, sharex='col', figsize=(self.fig_width, self.fig_height))
+        plt.suptitle("Time Series LWrist and HR Intensity Data (invalid ECG removed = {})"
+                     "\n(sleep = blue; LWrist nonwear = grey)".format(remove_invalid_hr))
+        ax1.plot(df["Timestamp"], df['Wrist_Intensity'], color='black')
+        ax1.set_ylabel("Intensity")
+        ax1.set_title("LWrist")
+
+        ax2.plot(df["Timestamp"], df["HR_Intensity"], color='red')
+        ax2.set_ylabel("Intensity")
+        ax2.set_title("Heart Rate")
+
+        xfmt = mdates.DateFormatter("%Y/%m/%d\n%H:%M:%S")
+        ax2.xaxis.set_major_formatter(xfmt)
+        plt.xticks(rotation=45, fontsize=8)
+
+        for row in self.df_event.itertuples():
+
+            if row.Event == "Sleep" and (start <= row.Start <= stop or start <= row.Stop <= stop):
+                ax1.fill_betweenx(x1=row.Start, x2=row.Stop, y=[0, 3],
+                                  color='navy', alpha=.5)
+                ax2.fill_betweenx(x1=row.Start, x2=row.Stop, y=[0, 3],
+                                  color='navy', alpha=.5)
+            if row.Event == "Nonwear" and (start <= row.Start <= stop or start <= row.Stop <= stop):
+                ax1.fill_betweenx(x1=row.Start, x2=row.Stop, y=[0, 3],
+                                  color='grey', alpha=.5)
+                ax2.fill_betweenx(x1=row.Start, x2=row.Stop, y=[0, 3],
+                                  color='grey', alpha=.5)
+
+        ax1.set_yticks(np.arange(0, 4, 1))
+        ax2.set_yticks(np.arange(0, 4, 1))
+
+        window_len = (stop - start).total_seconds()
+        ax2.set_xlim(start + timedelta(seconds=-window_len/20), stop + timedelta(seconds=window_len/20))
+
+    def calculate_activity_pattern(self, sort_by='hour', show_plot=True, save_csv=False):
+        """Calculates average counts and HR by either hour of day or by day of week. Generates histogram."""
+
+        print("\nAnalyzing activity trends by {}...".format(sort_by))
+
+        def sortby_hour():
+            accel_dict = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0,
+                           13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0}
+            accel_tally = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0,
+                          13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0}
+
+            hr_dict = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0,
+                          13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0}
+            hr_tally = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0, 9: 0, 10: 0, 11: 0, 12: 0,
+                       13: 0, 14: 0, 15: 0, 16: 0, 17: 0, 18: 0, 19: 0, 20: 0, 21: 0, 22: 0, 23: 0}
+
+            for row in self.df_epoch.itertuples():
+                if row.Wear_Status == "Wear":
+                    accel_dict[row.Timestamp.hour] += row.LWrist
+                    accel_tally[row.Timestamp.hour] += 1
+
+                    if row.ECG_Validity == "Valid":
+                        hr_dict[row.Timestamp.hour] += row.HR
+                        hr_tally[row.Timestamp.hour] += 1
+
+            accel_averages = [s / t if t != 0 else 0 for s, t in zip(accel_dict.values(), accel_tally.values())]
+
+            hr_averages = [s / t if t != 0 else 0 for s, t in zip(hr_dict.values(), hr_tally.values())]
+
+            r = scipy.stats.pearsonr(accel_averages, hr_averages)
+            print("-Hourly correlation between average counts and HR: r = {}".format(round(r[0], 3)))
+
+            if show_plot:
+                fig, (ax1, ax2) = plt.subplots(2, figsize=(self.fig_width, self.fig_height))
+                plt.subplots_adjust(hspace=.25)
+                ax1.set_title("Average Hourly Activity Counts")
+                ax1.bar(accel_dict.keys(), accel_averages, align='center',
+                        edgecolor='black', color='grey', width=1, alpha=.75)
+                ax1.set_ylabel("Counts")
+                ax1.set_xticks(np.arange(0, 24, 1))
+
+                ax2.set_title("Average Hourly Heart Rate (invalid ECG removed)")
+                ax2.bar(hr_dict.keys(), hr_averages, align='center',
+                        edgecolor='black', color='red', width=1, alpha=.5)
+                ax2.axhline(self.rest_hr, color='black', linestyle='dashed', label="Resting HR")
+                ax2.set_ylim(self.rest_hr * .75, )
+                ax2.set_xticks(np.arange(0, 24, 1))
+                ax2.set_ylabel("HR (bpm)")
+                ax2.set_xlabel("Hour of Day")
+                ax2.legend()
+
+            if save_csv:
+                df = pd.DataFrame(list(zip(accel_dict.keys(), accel_averages, hr_averages)),
+                                  columns=["Hour", "Avg Counts", "Avg HR"])
+
+                print("\nSaving hourly activity data (Activity_by_hour.csv)")
+                df.to_csv("Activity_by_hour.csv", index=False)
+
+        def sortby_day():
+            # Day of week: Monday = 0, Sunday = 6
+            day_list = ["Monday", "Tus"]
+            accel_dict = {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0,
+                          "Friday": 0, "Saturday": 0, "Sunday": 0}
+            accel_tally = {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0,
+                          "Friday": 0, "Saturday": 0, "Sunday": 0}
+
+            hr_dict = {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0,
+                          "Friday": 0, "Saturday": 0, "Sunday": 0}
+            hr_tally = {"Monday": 0, "Tuesday": 0, "Wednesday": 0, "Thursday": 0,
+                          "Friday": 0, "Saturday": 0, "Sunday": 0}
+
+            for row in self.df_epoch.itertuples():
+                if row.Wear_Status == "Wear":
+                    accel_dict[row.Timestamp.day_name()] += row.LWrist
+                    accel_tally[row.Timestamp.day_name()] += 1
+
+                    if row.ECG_Validity == "Valid":
+                        hr_dict[row.Timestamp.day_name()] += row.HR
+                        hr_tally[row.Timestamp.day_name()] += 1
+
+            accel_averages = [s / t if t != 0 else 0 for s, t in zip(accel_dict.values(), accel_tally.values())]
+
+            hr_averages = [s / t if t != 0 else 0 for s, t in zip(hr_dict.values(), hr_tally.values())]
+
+            r = scipy.stats.pearsonr(accel_averages, hr_averages)
+            print("-Daily correlation between average counts and HR: r = {}".format(round(r[0], 3)))
+
+            if show_plot:
+                fig, (ax1, ax2) = plt.subplots(2, figsize=(self.fig_width, self.fig_height))
+                plt.subplots_adjust(hspace=.25)
+
+                ax1.set_title("Average Daily Activity Counts")
+                ax1.bar(accel_dict.keys(), accel_averages, align='center',
+                        edgecolor='black', color='grey', width=1, alpha=.75)
+                ax1.set_ylabel("Counts")
+
+                ax2.set_title("Average Daily Heart Rate (invalid ECG removed)")
+                ax2.bar(hr_dict.keys(), hr_averages, align='center',
+                        edgecolor='black', color='red', width=1, alpha=.5)
+                ax2.axhline(self.rest_hr, color='black', linestyle='dashed', label="Resting HR")
+                ax2.set_ylim(self.rest_hr * .75, )
+                ax2.set_ylabel("HR (bpm)")
+                ax2.set_xlabel("Hour of Day")
+                ax2.legend()
+
+            if save_csv:
+                df = pd.DataFrame(list(zip(accel_dict.keys(), accel_averages, hr_averages)),
+                                  columns=["Day", "Avg Counts", "Avg HR"])
+
+                print("\nSaving hourly activity data (Activity_by_hour.csv)")
+                df.to_csv("Activity_by_day.csv", index=False)
+
+        if sort_by == "hour":
+            sortby_hour()
+        if sort_by == "day":
+            sortby_day()
 
 
 x = Subject(processed_filename="/Users/kyleweber/Desktop/Python Scripts/WearablesCourse/Data Files/Lab 9/Lab9_Epoched.csv",
@@ -194,3 +371,10 @@ x = Subject(processed_filename="/Users/kyleweber/Desktop/Python Scripts/Wearable
 
 # Plots LWrist counts and HR. Sleep/nonwear periods shaded.
 # x.plot_time_series(start=None, stop=None)
+
+# Plots LWrist and HR intensity. Sleep/nonwear periods shaded
+# x.compare_wrist_hr_intensity(remove_invalid_hr=False, start="2020-03-03 14:00:00", stop="2020-03-03 15:00:00")
+
+# Analyze activity patterns by 'day' or by 'hour'
+# x.calculate_activity_pattern(sort_by='hour', show_plot=True, save_csv=True)
+
